6c10e4fdf5c06973b082d3a38ad6cb1ccae18a0e
Florian Sesser <florian@leastauthority.com>
2021-01-17 13:22:02 +0000

    copy network.nix example from nixos-build-vms man page

---
+++ network.nix
@@ -0,0 +1,21 @@
+{
+  test1 = {pkgs, config, ...}:
+    {
+      services.openssh.enable = true;
+      nixpkgs.localSystem.system = "i686-linux";
+      deployment.targetHost = "test1.example.net";
+
+      # Other NixOS options
+    };
+
+  test2 = {pkgs, config, ...}:
+    {
+      services.openssh.enable = true;
+      services.httpd.enable = true;
+      environment.systemPackages = [ pkgs.lynx ];
+      nixpkgs.localSystem.system = "x86_64-linux";
+      deployment.targetHost = "test2.example.net";
+
+      # Other NixOS options
+    };
+}
