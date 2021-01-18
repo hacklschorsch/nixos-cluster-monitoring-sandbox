042f99056d6b740ca303ca47ece73e6dfc021fec
Florian Sesser <florian@leastauthority.com>
2021-01-18 14:14:47 +0000

    Add prometheus node exporter

--- network.nix
+++ network.nix
@@ -2,13 +2,8 @@
   test1 = {pkgs, config, ...}:
     {
       services.openssh.enable = true;
-      # nixpkgs.localSystem.system = "i686-linux";
       # deployment.targetHost = "test1.example.net";
 
-      # Other NixOS options
-      #testScript = ''
-      #  $machine->succeed("ip addr")
-      #'';
     };
 
   test2 = {pkgs, config, ...}:
@@ -19,6 +14,20 @@
       # nixpkgs.localSystem.system = "x86_64-linux";
       # deployment.targetHost = "test2.example.net";
 
-      # Other NixOS options
+services.prometheus.exporters.node = {
+    enable = true;
+    enabledCollectors = [
+      "logind"
+      "systemd"
+    ];
+    disabledCollectors = [
+      "textfile"
+    ];
+    # TODO: Open firewall to prometheus host
+    # openFirewall = true;
+    # firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
+  };
+
+
     };
 }
