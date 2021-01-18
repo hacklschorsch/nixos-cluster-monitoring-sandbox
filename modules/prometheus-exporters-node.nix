d4357dcfce7662a92ca58c4f7a04bfb5a7663169
Florian Sesser <florian@leastauthority.com>
2021-01-18 23:37:29 +0000

    a bit prettier

--- modules/prometheus-exporters-node.nix
+++ modules/prometheus-exporters-node.nix
@@ -1,7 +1,7 @@
 # This is our common node exporter config for our monitoring setup.
 
 # Thx https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters
-{ pkgs, config, options, ... }:
+{ config, ... }:
 
 let
 
@@ -14,17 +14,19 @@ in
   # cfg.enable = true;
   # but that fails with an error:
   # The option `cfg' defined in `...' does not exist.
-  config.services.prometheus.exporters.node.enable = true;
 
-  config.services.prometheus.exporters.node.enabledCollectors = [
-    "logind"
-    "systemd"
-  ];
-  config.services.prometheus.exporters.node.disabledCollectors = [
-    "textfile"
-  ];
-  # TODO: Open firewall only to prometheus host
-  # openFirewall = true;
-  # firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
+  config.services.prometheus.exporters.node = {
+    enable = true;
+    enabledCollectors = [
+      "logind"
+      "systemd"
+    ];
+    disabledCollectors = [
+      "textfile"
+    ];
+    # TODO: Open firewall only to prometheus host
+    openFirewall = true;
+    firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
+  };
 }
 
