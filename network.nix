e2cf38cef2532fd0b3f7d855b51be13aa430df23
Florian Sesser <florian@leastauthority.com>
2021-01-19 12:38:00 +0000

    clean up inb4 I show to JP

--- network.nix
+++ network.nix
@@ -3,51 +3,44 @@
 # Scope: Network of virtual host to develop and test monitoring setup
 
 # TODO:
-#  - DRY
 #  - configure monitoring software to actually monitor stuff, not just sit there
 #    - prometheus
 #    - loki
 #  - create beautiful dashboards
+#  - Should we monitor the monitoring hosts as well? I think yes
+#    - DRY
 
 {
+  # Monitoring infrastructure
+
   prometheus = {
-      imports = [
-        ./modules/prometheus.nix
-        ./modules/prometheus-exporters-node.nix
-        ./modules/promtail.nix
-      ];
-      services.openssh.enable = true;
-      # deployment.targetHost = "prometheus.example.net";
-    };
+    imports = [ ./modules/prometheus.nix ];
+    services.openssh.enable = true;
+    # deployment.targetHost = "prometheus.example.net";
+  };
 
   grafana = {
-      imports = [
-        ./modules/grafana.nix
-        ./modules/prometheus-exporters-node.nix
-        ./modules/promtail.nix
-      ];
-      services.openssh.enable = true;
-      # deployment.targetHost = "grafana.example.net";
-    };
+    imports = [ ./modules/grafana.nix ];
+    services.openssh.enable = true;
+    # deployment.targetHost = "grafana.example.net";
+  };
 
   loki = {
-      imports = [
-        ./modules/loki.nix
-        ./modules/prometheus-exporters-node.nix
-        ./modules/promtail.nix
-      ];
-      services.openssh.enable = true;
-      # deployment.targetHost = "loki.example.net";
-    };
+    imports = [ ./modules/loki.nix ];
+    services.openssh.enable = true;
+    # deployment.targetHost = "loki.example.net";
+  };
 
+
+  # Monitored machines
 
   node1 = {
-      imports = [
-        ./modules/prometheus-exporters-node.nix
-        ./modules/promtail.nix
-      ];
-      services.openssh.enable = true;
-      # deployment.targetHost = "node1.example.net";
-    };
+    imports = [
+      ./modules/prometheus-exporters-node.nix
+      ./modules/promtail.nix
+    ];
+    services.openssh.enable = true;
+    # deployment.targetHost = "node1.example.net";
+  };
 }
 
