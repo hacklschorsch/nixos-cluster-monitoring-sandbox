00234906755650bfe8373769957681fd6b8f8cbb
Florian Sesser <florian@leastauthority.com>
2021-01-19 12:30:53 +0000

    Clean up a bit, see that I need to DRY; add todos

--- network.nix
+++ network.nix
@@ -2,30 +2,52 @@
 #
 # Scope: Network of virtual host to develop and test monitoring setup
 
+# TODO:
+#  - DRY
+#  - configure monitoring software to actually monitor stuff, not just sit there
+#    - prometheus
+#    - loki
+#  - create beautiful dashboards
+
 {
-  test1 = {
-      imports = [ ./modules/prometheus.nix ./modules/prometheus-exporters-node.nix ];
+  prometheus = {
+      imports = [
+        ./modules/prometheus.nix
+        ./modules/prometheus-exporters-node.nix
+        ./modules/promtail.nix
+      ];
       services.openssh.enable = true;
-      # deployment.targetHost = "test1.example.net";
+      # deployment.targetHost = "prometheus.example.net";
     };
 
-  test2 = {
-      imports = [ ./modules/grafana.nix ./modules/prometheus-exporters-node.nix ];
+  grafana = {
+      imports = [
+        ./modules/grafana.nix
+        ./modules/prometheus-exporters-node.nix
+        ./modules/promtail.nix
+      ];
       services.openssh.enable = true;
-      # deployment.targetHost = "test2.example.net";
+      # deployment.targetHost = "grafana.example.net";
     };
 
-  test3 = {
-      imports = [ ./modules/loki.nix ./modules/prometheus-exporters-node.nix ];
+  loki = {
+      imports = [
+        ./modules/loki.nix
+        ./modules/prometheus-exporters-node.nix
+        ./modules/promtail.nix
+      ];
       services.openssh.enable = true;
-      # deployment.targetHost = "test3.example.net";
+      # deployment.targetHost = "loki.example.net";
     };
 
 
-  test10 = {
-      imports = [ ./modules/prometheus-exporters-node.nix ./modules/promtail.nix ];
+  node1 = {
+      imports = [
+        ./modules/prometheus-exporters-node.nix
+        ./modules/promtail.nix
+      ];
       services.openssh.enable = true;
-      # deployment.targetHost = "test10.example.net";
+      # deployment.targetHost = "node1.example.net";
     };
 }
 
