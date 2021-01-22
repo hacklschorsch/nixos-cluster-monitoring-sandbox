c0bf3f529c07824559841a0449656e92b8d2d93e
Florian Sesser <florian@leastauthority.com>
2021-01-22 13:30:32 +0000

    prometheus and loki work

--- network.nix
+++ network.nix
@@ -3,9 +3,6 @@
 # Scope: Network of virtual host to develop and test monitoring setup
 
 # TODO:
-#  - configure monitoring software to actually monitor stuff, not just sit there
-#    - prometheus
-#    - loki
 #  - create beautiful dashboards
 #  - Should we monitor the monitoring hosts as well? I think yes
 #    - DRY
