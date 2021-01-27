72474fca815bd730c7cc3624642e84dc2fae4fa8
Florian Sesser <florian@leastauthority.com>
2021-01-27 23:17:42 +0000

    strings for syntax highlighting, clearer node names

--- network.nix
+++ network.nix
@@ -9,37 +9,37 @@
 {
   # Monitoring infrastructure
 
-  prometheus = {
+  "prometheus" = {
     imports = [ ./modules/prometheus.nix ];
     # TODO: What is the right place for the firewall configs?
     # How to best also limit the hosts that can access the ports?
     networking.firewall.allowedTCPPorts = [ 9001 ];
-    # deployment.targetHost = "prometheus.example.net";
+    # deployment.targetHost = "prometheus.grid.private.storage";
   };
 
-  grafana = {
+  "grafana" = {
     imports = [ ./modules/grafana.nix ];
     networking.firewall.allowedTCPPorts = [ 80 2342 ];
-    # deployment.targetHost = "grafana.example.net";
+    # deployment.targetHost = "grafana.grid.private.storage";
   };
 
-  loki = {
+  "loki" = {
     imports = [ ./modules/loki.nix ];
     networking.firewall.allowedTCPPorts = [ 3100 ];
-    # deployment.targetHost = "loki.example.net";
+    # deployment.targetHost = "loki.grid.private.storage";
   };
 
 
   # Monitored machines
 
-  node1 = {
+  "node1" = {
     imports = [
       ./modules/prometheus-exporters-node.nix
       ./modules/promtail.nix
     ];
     networking.firewall.allowedTCPPorts = [ 9100 ];
     services.openssh.enable = true;
-    # deployment.targetHost = "node1.example.net";
+    # deployment.targetHost = "node1.grid.private.storage";
   };
 }
 
